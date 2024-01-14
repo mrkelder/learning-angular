71d4c3e0473c456c8c72b3bffd179c3bcfc2ab60
Vlad Aheiev <vageev06@gmail.com>
2024-01-14 15:03:42 -0500

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { StatisticsPageComponent } from './pages/statistics-page/statistics-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Statistics',
+    component: StatisticsPageComponent,
+    path: 'statistics',
+  },
+  {
+    title: 'Welcome',
+    component: HomePageComponent,
+    path: '',
+  },
+];
---
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-page',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './statistics-page.component.html',
+  styleUrl: './statistics-page.component.css',
+})
+export class StatisticsPageComponent {}
